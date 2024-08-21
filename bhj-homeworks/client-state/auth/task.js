05d7c265ac39d64be2dd294a9cef68da186d806c
Egor Velikiy <egor.velikiy.02@mail.ru>
2024-08-21 23:37:57 +0300

    storage2

--- bhj-homeworks/client-state/auth/task.js
+++ bhj-homeworks/client-state/auth/task.js
@@ -1,7 +1,27 @@
 const myFrom = document.getElementById('signin__form')
-const name = document.getElementsByName('login')
+const user_name = document.getElementsByName('login')
 const password = document.getElementsByName('password')
-const id = document.getElementById('user_id')
+const user_id = document.getElementById('user_id')
+const signin = document.querySelector('.signin')
+const welcome = document.querySelector('.welcome')
+
+function saveObject(key, object) {
+    localStorage.setItem(key, JSON.stringify(object));
+};
+
+function restoreObject(key) {
+    return JSON.parse(localStorage.getItem(key));
+};
+
+
+function countID(initialValue) {
+    counter = initialValue;
+    return () => {
+        counter++;
+    }
+};
+
+let idResult = countID(1)
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -9,10 +29,30 @@ myForm.addEventListener('submit', (e) => {
     const xhr = new XMLHttpRequest();
 
     xhr.onload = () => {
-        
-    }
+        const storedUser = restoreObject(user_name);
+
+        if (storedUser) {
+            if (storedUser['password'] === password && storedUser['login'] === user_name) {
+                signin.className = 'signin';
+                welcome.className = 'welcome welcome_active';
+                user_id.textContent = user['id'];
+            } else {
+                alert('Неверный логин/пароль');
+            }
+        } else {
+            const user = {
+                id: idResult(),
+                login: user_name,
+                password: password,
+            }
+            saveObject(user_name, user);
+            signin.className = 'signin';
+            welcome.className = 'welcome welcome_active';
+            user_id.textContent = user['id'];
+        }
+    };
 
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth')
-
+    
     xhr.send(formData)
 });
